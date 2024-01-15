18162bc3f51985d01cb216b4c80ab454e7f16470
timjuic <tim.juic@gmail.com>
2024-01-15 18:22:03 +0100

    Add express server for serving page

--- main.js
+++ main.js
@@ -1,28 +1,31 @@
 const express = require('express');
-const http = require('http');
+// const http = require('http');
 const socketIo = require('socket.io');
 const kafka = require('kafka-node');
-require('dotenv').config();  // Load environment variables from .env file
+const path = require('path');
+require('dotenv').config();
 
 const kafkaHost = process.env.KAFKA_BROKER || 'localhost:9092';
 
 const app = express();
-const server = http.createServer(app);
+const server = app.listen(3000, () => {
+    console.log('WebSocket server listening on *:3000');
+});
 const io = socketIo(server);
 
 // Kafka Consumer
 const Consumer = kafka.Consumer;
 const client = new kafka.KafkaClient({ kafkaHost: kafkaHost });
-
 const topics = [
     { topic: 'temperature-data' },
     { topic: 'motion-sensor-data' },
     { topic: 'door-sensor-data' },
     { topic: 'vibration-sensor-data' },
 ];
-
 const options = { autoCommit: true, groupId: 'web-interface-group' };
 const consumer = new Consumer(client, topics, options);
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 // WebSocket Connection
 io.on('connection', (socket) => {
@@ -40,7 +43,3 @@ consumer.on('message', (message) => {
     console.log(message.value)
 });
 
-// Start WebSocket server
-server.listen(3000, () => {
-    console.log('WebSocket server listening on *:3000');
-});
